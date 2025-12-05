f5f14dc772cdf3c70965387fdc5983c90dfef75a
Andrea Petrucci <andrea.petrucci@hefr.ch>
2025-12-05 13:23:14 +0100

    Change model ID from stable-diffusion to sd-turbo

--- src/main.py
+++ src/main.py
@@ -33,7 +33,7 @@ settings = get_settings()
 negative_prompts = "font++, typo++, signature, text++, watermark++, cropped, disfigured, duplicate, error, " \
                    "jpeg artifacts, low quality, lowres, mutated hands, out of frame, worst quality"
 
-MODEL_ID = "stabilityai/stable-diffusion-xl-base-1.0"
+MODEL_ID = "stabilityai/sd-turbo"
 GUIDANCE_SCALE = 5
 
 
